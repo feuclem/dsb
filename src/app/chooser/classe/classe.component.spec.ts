7bbe2f361c7d5a8868e020861d72d233527438ab
OCTO-CLEM <tclement@octo.com>
2019-08-24 15:13:49 +0200

    Test: chooser classe + niveau

--- src/app/chooser/classe/classe.component.spec.ts
+++ src/app/chooser/classe/classe.component.spec.ts
@@ -7,12 +7,15 @@ describe('ClasseComponent', () => {
   let component: ClasseComponent
   let fixture: ComponentFixture<ClasseComponent>
 
+  let classeService = null
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ClasseComponent],
       providers: [ClasseService]
     })
-      .compileComponents()
+
+    classeService = TestBed.get(ClasseService)
   }))
 
   beforeEach(() => {
@@ -21,7 +24,35 @@ describe('ClasseComponent', () => {
     fixture.detectChanges()
   })
 
-  it('should create', () => {
-    expect(component).toBeTruthy()
+  describe('on init', () => {
+    it('should set value', () => {
+      expect(component.classes).toEqual([
+          'Iop',
+          'Sacrieur'
+        ]
+      )
+      expect(component.currentClasse).toEqual('')
+    })
+  })
+
+  describe('selectClasse', () => {
+    it('should set current classe with the given data', () => {
+      // When
+      component.selectClasse('fakeClasse')
+
+      // Then
+      expect(component.currentClasse).toEqual('fakeClasse')
+    })
+
+    it('should call niveau servivce with the given data', () => {
+      // Given
+      const spy = spyOn(classeService, 'updateClasse')
+
+      // When
+      component.selectClasse('fakeClasse')
+
+      // Then
+      expect(spy).toHaveBeenCalledWith('fakeClasse')
+    })
   })
 })
--- src/app/chooser/niveau/niveau.component.spec.ts
+++ src/app/chooser/niveau/niveau.component.spec.ts
@@ -7,12 +7,15 @@ describe('NiveauComponent', () => {
   let component: NiveauComponent
   let fixture: ComponentFixture<NiveauComponent>
 
+  let niveauService = null
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [NiveauComponent],
       providers: [NiveauService]
     })
-      .compileComponents()
+
+    niveauService = TestBed.get(NiveauService)
   }))
 
   beforeEach(() => {
@@ -21,7 +24,36 @@ describe('NiveauComponent', () => {
     fixture.detectChanges()
   })
 
-  it('should create', () => {
-    expect(component).toBeTruthy()
+  describe('on init', () => {
+    it('should create array of 200 integers, start 1, end 200', () => {
+      expect(component.niveaus.length).toEqual(200)
+      expect(component.niveaus[0]).toEqual(200)
+      expect(component.niveaus[199]).toEqual(1)
+    })
+
+    it('should set current niveau by default to 200', () => {
+      expect(component.currentNiveau).toEqual(200)
+    })
+  })
+
+  describe('selectNiveau', () => {
+    it('should set current niveau with the given data', () => {
+      // When
+      component.selectNiveau(1)
+
+      // Then
+      expect(component.currentNiveau).toEqual(1)
+    })
+
+    it('should call niveau servivce with the given data', () => {
+      // Given
+      const spy = spyOn(niveauService, 'updateNiveau')
+
+      // When
+      component.selectNiveau(1)
+
+      // Then
+      expect(spy).toHaveBeenCalledWith(1)
+    })
   })
 })
