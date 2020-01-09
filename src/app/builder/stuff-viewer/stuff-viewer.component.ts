75caf22a3517a7383f774de7ce8ca7747a88a041
OCTO-CLEM <tclement@octo.com>
2020-01-09 18:35:40 +0100

    Feat: add new feature to selecte stuff view

---
+++ src/app/builder/stuff-viewer/stuff-viewer.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core'
+import {StuffWatcherService} from '../stuff.watcher.service'
+import {Statistique} from '../../shared/entities/Statistique'
+
+@Component({
+  selector: 'dsb-stuff-viewer',
+  templateUrl: './stuff-viewer.component.html',
+  styles: []
+})
+export class StuffViewerComponent implements OnInit {
+
+  name: String
+  imgUrl: String
+  stats: Statistique[]
+
+  constructor(
+    private stuffWatcherService: StuffWatcherService
+  ) { }
+
+  ngOnInit() {
+    this.stuffWatcherService._selectedStuffToWatch.subscribe(value => {
+      if(value !== undefined) {
+        this.name = 'TODO'
+        this.imgUrl = value.imgUrl
+        this.stats = value.statistiques
+      }
+    })
+  }
+
+}
