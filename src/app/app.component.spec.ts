858bda048168868c892b87fb1634a0c8b963c779
ttclem <theo.clement.pro@gmail.com>
2020-11-27 16:43:18 +0100

    fix angular cli

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, TestBed} from '@angular/core/testing'
+import { TestBed, waitForAsync } from '@angular/core/testing'
 import {RouterTestingModule} from '@angular/router/testing'
 import {AppComponent} from './app.component'
 import {FormsModule} from '@angular/forms'
@@ -6,7 +6,7 @@ import {BuilderComponent} from './builder/builder.component'
 import {HttpClientTestingModule} from '@angular/common/http/testing'
 
 xdescribe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
@@ -19,7 +19,7 @@ xdescribe('AppComponent', () => {
       ]
     }).compileComponents()
   }))
-  it('should create the app', async(() => {
+  it('should create the app', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.debugElement.componentInstance
     expect(app).toBeTruthy()
